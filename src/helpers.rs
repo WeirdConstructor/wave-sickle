a578b7abc43a627563b81a682375788e4bd36c42
Weird Constructor <weirdconstructor@gmail.com>
2019-07-30 19:57:04 +0200

    more porting of SynthDevice, it compiles now

--- src/helpers.rs
+++ src/helpers.rs
@@ -78,43 +78,43 @@ pub fn fast_cos(mut x: f64) -> f64 {
     }
 }
 
-fn note_to_freq(note: f64) -> f64 {
+pub fn note_to_freq(note: f64) -> f64 {
     440.0 * pow(2.0, (note - 69.0) / 12.0)
 }
 
-fn db_to_scalar(db: f32) -> f32 {
+pub fn db_to_scalar(db: f32) -> f32 {
     powf(2.0, db / 6.0)
 }
 
-fn env_value_to_scalar(value: f32) -> f32 {
+pub fn env_value_to_scalar(value: f32) -> f32 {
     (value - 1.0).sqrt() / 5000.0
 }
 
-fn scalar_to_env_value(scalar: f32) -> f32 {
+pub fn scalar_to_env_value(scalar: f32) -> f32 {
     scalar * scalar * 5000.0 + 1.0
 }
 
-fn volume_to_scalar(volume: f32) -> f32 {
+pub fn volume_to_scalar(volume: f32) -> f32 {
     let v = volume * 0.4;
     v * v
 }
 
-fn scalar_to_volume(scalar: f32) -> f32 {
+pub fn scalar_to_volume(scalar: f32) -> f32 {
     scalar.sqrt() / 0.4
 }
 
-fn param_to_boolean(value: f32) -> bool { value >= 0.5 }
-fn boolean_to_param(b: bool)    -> f32  { if b { 1.0 } else { 0.0 } }
+pub fn param_to_boolean(value: f32) -> bool { value >= 0.5 }
+pub fn boolean_to_param(b: bool)    -> f32  { if b { 1.0 } else { 0.0 } }
 
-fn param_to_frequency(param: f32) -> f32 {
+pub fn param_to_frequency(param: f32) -> f32 {
     20.0 + (20000.0 - 20.0) * param * param
 }
 
-fn frequency_to_param(freq: f32) -> f32 {
+pub fn frequency_to_param(freq: f32) -> f32 {
     ((freq - 20.0) / (20000.0 - 20.0)).sqrt()
 }
 
-fn param_to_q(param: f32) -> f32 {
+pub fn param_to_q(param: f32) -> f32 {
     if param < 0.5 {
         param / 0.5 * (1.0 - 0.33) + 0.33
     } else {
@@ -122,7 +122,7 @@ fn param_to_q(param: f32) -> f32 {
     }
 }
 
-fn q_to_param(q: f32) -> f32 {
+pub fn q_to_param(q: f32) -> f32 {
     if q < 1.0 {
         (q - 0.33) / (1.0 - 0.33) * 0.5
     } else {
@@ -130,44 +130,44 @@ fn q_to_param(q: f32) -> f32 {
     }
 }
 
-fn param_to_db(param: f32, range: f32) -> f32 {
+pub fn param_to_db(param: f32, range: f32) -> f32 {
     (param * 2.0 - 1.0) * range
 }
 
-fn db_to_param(db: f32, range: f32) -> f32 {
+pub fn db_to_param(db: f32, range: f32) -> f32 {
     (db / range + 1.0) / 2.0
 }
 
-fn param_to_resonance(param: f32) -> f32 {
+pub fn param_to_resonance(param: f32) -> f32 {
     param * 0.99 + 0.01
 }
 
-fn resonance_to_param(resonance: f32) -> f32 {
+pub fn resonance_to_param(resonance: f32) -> f32 {
     (resonance - 0.01) / 0.99
 }
 
-fn param_to_unisono(param: f32) -> i32 {
+pub fn param_to_unisono(param: f32) -> i32 {
     (param * 15.0) as i32 + 1
 }
 
-fn unisono_to_param(unisono: i32) -> f32 {
+pub fn unisono_to_param(unisono: i32) -> f32 {
     (unisono - 1) as f32 / 15.0
 }
 
-fn param_to_vibrato_freq(param: f32) -> f64 {
+pub fn param_to_vibrato_freq(param: f32) -> f64 {
     (pow(param as f64, 2.0) + 0.1) * 70.0
 }
 
-fn vibrato_freq_to_param(vf: f64) -> f32 {
+pub fn vibrato_freq_to_param(vf: f64) -> f32 {
     let d = vf / 70.0 - 0.1;
     if d >= 0.0 { d.sqrt() as f32 } else { 0.0 }
 }
 
-fn pan_to_scalar_left(pan: f32) -> f32 {
+pub fn pan_to_scalar_left(pan: f32) -> f32 {
     (1.0 - pan).sqrt()
 }
 
-fn pan_to_scalar_right(pan: f32) -> f32 {
+pub fn pan_to_scalar_right(pan: f32) -> f32 {
     pan.sqrt()
 }
 
--- src/synth_device.rs
+++ src/synth_device.rs
@@ -1,10 +1,41 @@
 use crate::parameters::*;
 use crate::helpers;
 
+#[derive(Debug, PartialEq, Copy, Clone)]
+enum EventType {
+    None,
+    NoteOn,
+    NoteOff,
+}
+
+#[derive(Debug, PartialEq, Copy, Clone)]
+struct Event {
+    typ:           EventType,
+    delta_samples: i32,
+    note:          i32,
+    velocity:      i32,
+}
+
+impl Event {
+    fn new() -> Event {
+        Event {
+            typ:           EventType::None,
+            delta_samples: 0,
+            note:          0,
+            velocity:      0,
+        }
+    }
+
+    fn clear(&mut self) {
+        self.typ = EventType::None;
+    }
+}
+
 trait ParameterSet {
     fn enumerate(&self) -> Vec<Parameter>;
 }
 
+#[derive(Debug, PartialEq, Copy, Clone)]
 struct VoiceData {
     pub sample_rate:      f64,
     pub is_on:            bool,
@@ -26,7 +57,7 @@ impl VoiceData {
             is_on:            false,
             note:             0,
             detune:           0.0,
-            pan:              0.0,
+            pan:              0.5,
             vibrato_phase:    0.0,
             slide_active:     false,
             slide_delta:      0.0,
@@ -48,11 +79,13 @@ impl VoiceData {
     fn note_off(&mut self) {
     }
 
-    fn note_slide(&mut self, data: &SynthData, note: i32) {
+    fn note_slide<V>(&mut self, data: &SynthDevice<V>, note: i32)
+        where V: Voice + ParameterSet {
+
         self.slide_active     = true;
         self.destination_note = note;
 
-        let slide_time        = 10.0 * helpers::pow(data.slide, 4.0);
+        let slide_time        = 10.0 * helpers::pow(data.slide as f64, 4.0);
         self.slide_delta      = (note as f64 - self.current_note)
                                 / self.sample_rate * slide_time;
         self.slide_samples    = (self.sample_rate * slide_time) as i32;
@@ -73,7 +106,8 @@ impl VoiceData {
     }
 }
 
-trait Voice {
+trait Voice: Copy + Clone {
+    fn new(sample_rate: f64) -> Self;
     fn note_on(&mut self, data: &mut VoiceData, note: i32, velocity: i32, detune: f32, pan: f32);
     fn note_off(&mut self, data: &mut VoiceData);
     fn note_slide(&mut self, data: &mut VoiceData, note: i32);
@@ -81,26 +115,59 @@ trait Voice {
     fn run(&mut self, data: &mut VoiceData, song_pos: f64, param: &mut ParameterData, outputs: &mut [f64]);
 }
 
-struct SynthData {
+struct SynthDevice<V>
+    where V: Voice + ParameterSet {
+
+    sample_rate:    f64,
     voices_unisono: i32,
-    voices_detune: f32,
-    voices_pan: f32,
-    vibrato_freq: f64,
+    voices_detune:  f32,
+    voices_pan:     f32,
+    vibrato_freq:   f64,
     vibrato_amount: f32,
-    rise: f32,
-    slide: f32,
+    rise:           f32,
+    slide:          f32,
+    voice_mode:     VoiceMode,
+    mono_active:    bool,
+    note_log:       [i32; 128],
+    note_count:     i32,
+    active_notes:   [bool; 128],
+    voice_data:     [VoiceData; 256],
+    voices:         [V; 256],
+    events:         [Event; 256],
 }
 
-struct SynthDevice<T, V>
-    where V: Voice + ParameterSet {
-    voice_data:   [VoiceData; 256],
-    voices:       [V; 256],
-    mono_active:  bool,
-    note_log:     [i32; 128],
-    note_count:   i32,
-    active_notes: [bool; 128],
-    voice_mode:   VoiceMode,
+impl<V: Voice + ParameterSet> SynthDevice<V> {
+    fn new(sample_rate: f64) -> Self {
+        SynthDevice {
+            sample_rate,
+            voices_unisono: 1,
+            voices_detune:  0.0,
+            voices_pan:     0.5,
+            vibrato_freq:   helpers::param_to_vibrato_freq(0.0),
+            vibrato_amount: 0.0,
+            rise:           0.0,
+            slide:          0.0,
+            voice_mode:     VoiceMode::Polyphonic,
+            mono_active:    false,
+            note_count:     0,
+            active_notes:   [false; 128],
+            note_log:       [0; 128],
+            voice_data:     [VoiceData::new(sample_rate); 256],
+            voices:         [V::new(sample_rate); 256],
+            events:         [Event::new(); 256],
+        }
+    }
+
+    fn clear_events(&mut self) {
+        for e in self.events.iter_mut() { e.clear(); }
+    }
 }
+
+//struct SynthDevice<V>
+//    where V: Voice + ParameterSet {
+//    voice_data:   [VoiceData; 256],
+//    voices:       [V; 256],
+//}
 
 // SynthDevice -> Manages voices and note on/off events
 // Slaughter   -> Holds parameters
